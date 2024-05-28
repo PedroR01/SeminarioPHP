4db64dad2ed067dd79b9ee815bf426c533d4d4fc
Pedro <peporobinet01@gmail.com>
2024-05-28 16:44:55 -0300

    React frontend app

---
+++ Frontend/inmobiliaria/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-darks"></nav>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/inmobiliaria/src/pages/localidad/LocalidadPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LocalidadPage() {
+  return <h1>Localidad Page</h1>;
+}
